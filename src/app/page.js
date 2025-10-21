e5e27034863cf95449caa727f03623e413d2d49a
Alfonso Dou <alfonsodou@gmail.com>
2025-10-21 19:02:08 +0200

    Configuración red blockchain

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,52 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useState, useRef, useEffect } from "react";
+import detectEthereumProvider from "@metamask/detect-provider"
+import subastaManifest from "../contracts/Subasta.json"
+import { ethers, Contract } from "ethers"
+import { decodeError } from "@ubiquity-os/ethers-decode-error";
+import { Container } from "react-bootstrap";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const myContract = useRef(null);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  useEffect( () => {
+    let init = async () => {
+      await configurarBlockchain();
+    }
+    init();
+  }, [])
+
+ /**
+ * Configura la red blockchain y carga el contrato Subasta
+ *
+ * @returns {void}
+ */
+  const configurarBlockchain = async () => {
+    try {
+      const provider = await detectEthereumProvider();
+
+      if (provider) {
+        await provider.request({method: "eth_requestAccounts"});
+        let providerEthers = new ethers.providers.Web3Provider(provider);
+        let signer = providerEthers.getSigner();
+        myContract.current = new Contract(
+          "0x60a392e720e54b80405a9a95878de57a5416099e",
+          subastaManifest.abi,
+          signer
+        );
+  
+      } else {
+        console.log("No se puede conectar con el provider")
+      }  
+    } catch (err) {
+      const error = decodeError(err);
+      alert(error.error);
+    }
+  }
+
+  return (
+    <Container>
+
+    </Container>
   );
 }
