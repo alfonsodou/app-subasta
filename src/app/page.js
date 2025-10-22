f66302cdc51ad26010d2ec5b01e2214c83f31152
Alfonso Dou <alfonsodou@gmail.com>
2025-10-22 12:38:39 +0200

    Función para actualizar el estado de la subasta

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,15 @@
 "use client";
 import { useState, useRef, useEffect } from "react";
-import detectEthereumProvider from "@metamask/detect-provider"
-import subastaManifest from "../contracts/Subasta.json"
-import { ethers, Contract } from "ethers"
+import detectEthereumProvider from "@metamask/detect-provider";
+import subastaManifest from "../contracts/Subasta.json";
+import { ethers, Contract } from "ethers";
 import { decodeError } from "@ubiquity-os/ethers-decode-error";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-import Alert from 'react-bootstrap/Alert';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Home() {
   const myContract = useRef(null);
@@ -21,25 +21,25 @@ export default function Home() {
   const [bid, setBid] = useState(0);
   const [active, setActive] = useState(0);
 
-  useEffect( () => {
+  useEffect(() => {
     let init = async () => {
       await configurarBlockchain();
       await cargarDatos();
-    }
+    };
     init();
-  }, [])
+  }, []);
 
- /**
- * Configura la red blockchain y carga el contrato Subasta
- *
- * @returns {void}
- */
+  /**
+   * Configura la red blockchain y carga el contrato Subasta
+   *
+   * @returns {void}
+   */
   const configurarBlockchain = async () => {
     try {
       const provider = await detectEthereumProvider();
 
       if (provider) {
-        await provider.request({method: "eth_requestAccounts"});
+        await provider.request({ method: "eth_requestAccounts" });
         let providerEthers = new ethers.providers.Web3Provider(provider);
         let signer = providerEthers.getSigner();
         myContract.current = new Contract(
@@ -47,34 +47,33 @@ export default function Home() {
           subastaManifest.abi,
           signer
         );
-  
       } else {
-        console.log("No se puede conectar con el provider")
-      }  
+        console.log("No se puede conectar con el provider");
+      }
     } catch (err) {
       const error = decodeError(err);
       alert(error.error);
     }
-  }
+  };
 
   /**
-  * Almacena los datos del contrato
-  *
-  * @returns {void}
-  */
+   * Almacena los datos del contrato
+   *
+   * @returns {void}
+   */
   const cargarDatos = async () => {
-   try {  
+    try {
       let descriptionTemp = await myContract.current.description();
       setDescription(descriptionTemp);
 
       let deadlineMinutes = await myContract.current.deadLine();
       let deadlineDate = new Date(deadlineMinutes * 1000);
-      setDeadLine(deadlineDate.toLocaleString());      
+      setDeadLine(deadlineDate.toString());
 
       let minBidWei = await myContract.current.minBid();
       let minBidBNB = ethers.utils.formatEther(minBidWei);
-      setMinBid(minBidBNB);    
-      
+      setMinBid(minBidBNB);
+
       await cargarDatosDinamicos();
     } catch (err) {
       const error = decodeError(err);
@@ -83,12 +82,12 @@ export default function Home() {
   };
 
   /**
-  * Almacena los datos dinámicos del contrato
-  *
-  * @returns {void}
-  */
+   * Almacena los datos dinámicos del contrato
+   *
+   * @returns {void}
+   */
   const cargarDatosDinamicos = async () => {
-    try {  
+    try {
       let maxBidWei = await myContract.current.maxBid();
       let maxBidBNB = ethers.utils.formatEther(maxBidWei);
       setMaxBid(maxBidBNB);
@@ -96,8 +95,20 @@ export default function Home() {
       let maxBidAddressTemp = await myContract.current.addressMaxBid();
       setMaxBidAddress(maxBidAddressTemp);
 
-      let activeTemp = await myContract.current.isActive()      
-      setActive(activeTemp);      
+      cargarEstadoSubasta();
+    } catch (err) {
+      const error = decodeError(err);
+      alert(error.error);
+    }
+  };
+
+  /**
+   * Actualiza el estado de la subasta
+   */
+  const cargarEstadoSubasta = async () => {
+    try {
+      let activeTemp = await myContract.current.isActive();
+      setActive(activeTemp);
     } catch (err) {
       const error = decodeError(err);
       alert(error.error);
@@ -109,20 +120,27 @@ export default function Home() {
    */
   let makeBid = async () => {
     try {
-      const tx = await myContract.current.makeBid({
-        value: ethers.utils.parseEther(bid)
-      })
-  
-      await tx.wait();
+      // Actualizamos el estado de la subasta
+      cargarEstadoSubasta();
+
+      // Si la subasta sigue activa
+      if (active) {
+        const tx = await myContract.current.makeBid({
+          value: ethers.utils.parseEther(bid),
+        });
+
+        await tx.wait();
+      }
+
       setBid(0);
-      
+
       // Actualizamos los datos del contrato que se han modificado
-      cargarDatosDinamicos();  
-    } catch(err) {
+      cargarDatosDinamicos();
+    } catch (err) {
       const error = decodeError(err);
       alert(error.error);
     }
-  }
+  };
 
   return (
     <Container>
@@ -135,20 +153,29 @@ export default function Home() {
           </Alert>
         </Col>
       </Row>
-      <h1>Description: {description}</h1>
-      <h1>DeadLine: {deadLine}</h1>
-      <h1>MinBid: {minBid}</h1>
-      <h1>MaxBid: {maxBid}</h1>
-      <h1>Address Max Bid: {maxBidAddress}</h1>
-      <h1>isActive: {active.toString()}</h1>
-      <input 
-        type="text" 
-        value={ bid } 
-        onChange={ (e) => setBid(e.target.value) }/>
-      <Button variant="primary" onClick={ () => { makeBid() }}>
-        Send
-      </Button>
+      <Row>
+        <Col>
+          <h1>Description: {description}</h1>
+          <h1>DeadLine: {deadLine}</h1>
+          <h1>MinBid: {minBid}</h1>
+          <h1>MaxBid: {maxBid}</h1>
+          <h1>Address Max Bid: {maxBidAddress}</h1>
+          <h1>isActive: {active.toString()}</h1>
+          <input
+            type="text"
+            value={bid}
+            onChange={(e) => setBid(e.target.value)}
+          />
+          <Button
+            variant="primary"
+            onClick={() => {
+              makeBid();
+            }}
+          >
+            Send
+          </Button>
+        </Col>
+      </Row>
     </Container>
   );
- 
 }
